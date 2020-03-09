a3d42ffaee6873b7fac3aa7ceba501eb75a95135
speak <speakk@gmail.com>
2020-03-09 23:23:14 +0200

    Day cycle indication GUI

--- models/ai/sharedActions/atTarget.lua
+++ models/ai/sharedActions/atTarget.lua
@@ -4,7 +4,7 @@ local universe = require('models.universe')
 return function()
   return BehaviourTree.Task:new({
   run = function(task, blackboard)
-    print("atTarget")
+    --print("atTarget")
     local gridPosition = universe.pixelsToGridCoordinates(blackboard.actor:get(ECS.c.position).vector)
     local targetGridPosition = universe.pixelsToGridCoordinates(blackboard.target:get(ECS.c.position).vector)
 
--- systems/dayCycle.lua
+++ systems/dayCycle.lua
@@ -1,26 +1,60 @@
+local settings = require 'settings'
 local DayCycleSystem = ECS.System({})
 
 local daySpeed = 0.5
 local currentTime = 0 -- Timer that runs and progresses world time
+local hour = 1 -- Derived from currentTime in timeChanged
 
 local lastEmit = 0
 local emitInterval = 0.01
+
+local cycleCanvas = love.graphics.newCanvas(100, 100)
+local cycleImage = love.graphics.newImage('media/misc/daycycle.png')
 
 function DayCycleSystem:update(dt) --luacheck: ignore
   currentTime = currentTime + daySpeed * dt
   local time = love.timer.getTime()
   if time - lastEmit > emitInterval then
     --print("Emit?", time)
-    self:getWorld():emit('timeOfDayChanged', self:getTimeOfDay(currentTime))
+    self:getWorld():emit('timeChanged', currentTime, self:getTimeOfDay(currentTime))
     lastEmit = time
   end
 end
 
+function DayCycleSystem:timeChanged(time, timeOfDay)
+  hour = math.ceil(timeOfDay * 24)
+
+  local rotation = timeOfDay * math.pi*2 - math.pi
+  love.graphics.push('all')
+  love.graphics.setBlendMode('alpha')
+  love.graphics.setCanvas({cycleCanvas, stencil = true})
+  love.graphics.setColor(1,1,1,1)
+  love.graphics.clear(0,0,0,0)
+  love.graphics.stencil(function()
+    love.graphics.setColor(1,1,1,1)
+    love.graphics.circle('fill', 50, 50, 50)
+  end, "replace", 1, false)
+  love.graphics.setStencilTest("greater", 0)
+  love.graphics.setScissor(0, 0, 100, 50)
+  love.graphics.setColor(1,1,1,1)
+  love.graphics.draw(cycleImage, 50, 50, rotation, 1, 1, 50, 50)
+  love.graphics.setStencilTest()
+  love.graphics.setCanvas()
+  love.graphics.pop()
+end
+
+function DayCycleSystem:generateGUIDraw()
+  local w, h = love.graphics.getDimensions()
+  love.graphics.setColor(1,1,1,1)
+  love.graphics.draw(cycleCanvas, w-120, h-settings.actions_bar_height-50)
+
+  love.graphics.print("H: " .. tostring(hour), w-80, h-settings.actions_bar_height-70)
+end
+
 -- Range: 0-1, 1 being midnight/morning, 0.5 middle of day
-function DayCycleSystem:getTimeOfDay() --luacheck: ignore
-  return math.sin(currentTime)
+function DayCycleSystem:getTimeOfDay(currentTime) --luacheck: ignore
+  print(currentTime % 1)
+  return currentTime * 0.1 % 1
 end
 
 return DayCycleSystem
-
-
